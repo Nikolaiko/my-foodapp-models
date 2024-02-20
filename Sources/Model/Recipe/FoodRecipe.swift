11dea9ba3d0ca77ca4b053b11ac51c160dfd42c4
Nikolai Baklanov <nikolaiko@mail.ru>
2024-02-20 09:10:11 +0600

    Made inits public

--- Sources/Model/Recipe/FoodRecipe.swift
+++ Sources/Model/Recipe/FoodRecipe.swift
@@ -7,4 +7,19 @@ public struct FoodRecipe {
     public let description: String
     public let products: [FoodRecipeProductEntry]
     public let tags: [Int]
+
+    public init(id: String,
+         name: String,
+         shortDescription: String,
+         description: String,
+         products: [FoodRecipeProductEntry],
+         tags: [Int]
+    ) {
+        self.id = id
+        self.name = name
+        self.shortDescription = shortDescription
+        self.description = description
+        self.products = products
+        self.tags = tags
+    }
 }
--- Sources/Model/Recipe/FoodRecipeProductEntry.swift
+++ Sources/Model/Recipe/FoodRecipeProductEntry.swift
@@ -5,4 +5,15 @@ public struct FoodRecipeProductEntry {
     public let productType: FoodProductType
     public let count: Int
     public let quantityMeasure: FoodQuantityType
+
+    public init(id: String,
+                productType: FoodProductType,
+                count: Int,
+                quantityMeasure: FoodQuantityType
+    ) {
+        self.id = id
+        self.productType = productType
+        self.count = count
+        self.quantityMeasure = quantityMeasure
+    }
 }
